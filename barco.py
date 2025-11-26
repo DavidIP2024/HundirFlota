350fe59ca0e896375720cf139ace12adefe53970
David <diglesiasperez@danielcastelao.org>
2025-11-26 13:18:14 +0100

    Nombre, longitud y golpes recibidos del barco

--- barco.py
+++ barco.py
@@ -1,3 +1,11 @@
 # Clase que representa un barco del juego Hundir la Flota
 class Barco:
     def __init__(self, nombre, longitud):
+        # Nombre del barco (ej: "Submarino", "Buque")
+        self.nombre = nombre
+
+        # Longitud del barco (cuántas casillas ocupa)
+        self.longitud = longitud
+
+        # Golpes recibidos por el barco (empieza en 0)
+        self.golpes_recibidos = 0
