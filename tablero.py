02645589d0a8affc842a8a8b3e206f61c945e70e
David <diglesiasperez@danielcastelao.org>
2025-11-26 13:24:09 +0100

    Inicialización del tablero con sus casillas

--- tablero.py
+++ tablero.py
@@ -3,3 +3,7 @@ class Tablero:
     def __init__(self, tamano):
         # El tablero será cuadrado: tamaño x tamaño
         self.dimensiones = (tamano, tamano)
+        # Inicialización de la matriz del tablero
+        # Todas las casillas empiezan como "agua" → representado con 0
+        filas, columnas = self.dimensiones
+        self.casillas = [[0 for _ in range(columnas)] for _ in range(filas)]
