18d8a26d00b5687179a8151b9a0c7ed1b473ec17
David <diglesiasperez@danielcastelao.org>
2025-11-26 13:23:22 +0100

    Tablero cuadrado

--- tablero.py
+++ tablero.py
@@ -1,3 +1,5 @@
 # Clase que representa el tablero del juego Hundir la Flota
 class Tablero:
     def __init__(self, tamano):
+        # El tablero será cuadrado: tamaño x tamaño
+        self.dimensiones = (tamano, tamano)
